606001e252687d7060b30b5b8f46016b95b8e14d
unknown <ocardenasmartinez@gmail.com>
2021-10-09 21:04:49 -0300

    add

--- src/main/java/cl/kids/controllers/ExceptionController.java
+++ src/main/java/cl/kids/controllers/ExceptionController.java
@@ -1,11 +1,14 @@
 package cl.kids.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
-    public void handleException(Exception e) {
+    public ResponseEntity<String> handleException(Exception e) {
         System.out.println(e);
+        return new ResponseEntity<>("NOK", HttpStatus.NOT_ACCEPTABLE);
     }
 }
